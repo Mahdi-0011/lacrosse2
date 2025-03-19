17bc743c3efa1c5447233474adfb237e427746f4
Niklas Mårdby <niklas.mardby@gmail.com>
2025-03-19 08:47:08 +0100

    Fix About link

--- src/routes/MonstersPage.tsx
+++ src/routes/MonstersPage.tsx
@@ -6,7 +6,7 @@ export default function MonstersPage() {
 	return (
 		<div>
 			<h1>MonstersPage</h1>
-			<Link to="/">Home</Link> - <Link to="about">About</Link>
+			<Link to="/">Home</Link> - <Link to="/about">About</Link>
 			<ul>
 				{monsters.map((monster) => (
 					<li key={monster}>
